56187140fa1db6c37fa0510040c1087890644686
Shehu-Yakubu <shehyak@gmail.com>
2022-09-01 10:25:13 +0000

    upload docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=shehyak/project-ml-microservice-kubernetes
 
 # Step 2:  
 # Authenticate & tag
+docker login -u shehyak
+docker tag project-ml-microservice-kubernetes ${dockerpath}
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push ${dockerpath}
